2ea93d9a20bd7f5c1bf7d79a0800fee13bd82e14
tsuboty <tsuboty@gmail.com>
2016-03-10 18:02:53 +0900

    usd

--- get.py
+++ get.py
@@ -2,40 +2,51 @@
 import urllib2
 from bs4 import BeautifulSoup
 from datetime import datetime
-from threading import Timer
+import threading
 
 
-# web source
-html = urllib2.urlopen("http://info.finance.yahoo.co.jp/fx")
-soup = BeautifulSoup(html,"html.parser")
+class CrawClass:
 
-# get timestamp
-d = datetime.today()
-timestanp = '%s-%s-%s %s:%s:%s' % (d.year,d.strftime('%m'),d.strftime('%d'),d.strftime('%H'), d.strftime('%M'), d.strftime('%S'))
+	def __init__(self):
+		# web source
+		html = urllib2.urlopen("http://info.finance.yahoo.co.jp/fx")
+		self.soup = BeautifulSoup(html,"html.parser")
 
-def innerHTML(element):
-    return element.decode_contents(formatter="html")
+	def getTimestamp(self):
+		d = datetime.today()
+		timestamp = '%s-%s-%s %s:%s:%s' % (d.year,d.strftime('%m'),d.strftime('%d'),d.strftime('%H'), d.strftime('%M'), d.strftime('%S'))
+		return timestamp
 
-def getUSDJPYBid():
-	bid_prince_tag = soup.div.find(id="USDJPY_top_bid")
-	bid = innerHTML(bid_prince_tag)
-	return bid
+	def innerHTML(self,element):
+	    return element.decode_contents(formatter="html")
 
-def write_log():
-	f = open("price.csv", 'a')
-	price = getUSDJPYBid()
-	log = timestanp + "," + price + "\n"
-	print log
-	f.write(log)
-	f.close()
+	def getUSDJPYBid(self):
 
+		bid_prince_tag = self.soup.div.find(id="USDJPY_top_bid")
+		bid = self.innerHTML(bid_prince_tag)
+
+		return bid
+
+	def write_log(self):
+		f = open("price.csv", 'a')
+		price = self.getUSDJPYBid()
+		log = self.getTimestamp() + "," + price + "\n"
+		print log
+		f.write(log)
+		f.close()
+
+# Thread loop
 def thread_write():
-	write_log()
-	t = Timer(5,thread_write)
+	c = CrawClass()
+	c.write_log()
+	t = threading.Timer(5,thread_write)
 	t.start()
+	del c
+
+
 
 thread_write()
-print d
+
 
 
 
