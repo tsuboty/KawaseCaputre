3aa54c69feb2fb6138cace0219aeb0da14acfeee
tsuboty <tsuboty@gmail.com>
2016-03-11 15:04:26 +0900

    add some currencies

--- get.py
+++ get.py
@@ -3,10 +3,14 @@ import urllib2
 from bs4 import BeautifulSoup
 from datetime import datetime
 import threading
+import os.path
+import collections
+
 
 
 class CrawClass:
-
+	currencies = ["USDJPY_top_bid","EURJPY_top_bid","GBPJPY_top_bid","AUDJPY_top_bid","NZDJPY_top_bid"]
+	title = "TIME,USDJPY,EURJPY,GBPJPY,AUDJPY,NZDJPY"
 	def __init__(self):
 		# web source
 		html = urllib2.urlopen("http://info.finance.yahoo.co.jp/fx")
@@ -20,18 +24,43 @@ class CrawClass:
 	def innerHTML(self,element):
 	    return element.decode_contents(formatter="html")
 
-	def getUSDJPYBid(self):
-
-		bid_prince_tag = self.soup.div.find(id="USDJPY_top_bid")
-		bid = self.innerHTML(bid_prince_tag)
-
-		return bid
-
+	def getUsdjpyBid(self):
+		usdjpy_tag = self.soup.div.find(id="USDJPY_top_bid")
+		usdjpy = self.innerHTML(usdjpy_tag)
+		return usdjpy
+    
+ 	def getBids(self):
+ 		result = collections.OrderedDict()
+ 		for c in self.currencies:
+ 			tag = self.soup.div.find(id=c)
+ 			result[c] = self.innerHTML(tag)
+ 		return result
+        
 	def write_log(self):
-		f = open("price.csv", 'a')
-		price = self.getUSDJPYBid()
-		log = self.getTimestamp() + "," + price + "\n"
+		t = datetime.today()
+		t = t.strftime('%Y-%m-%d')
+		file_name = t + "_price.csv"
+
+		# num_lines = sum(1 for line in open(file_name))
+		
+
+		# csv title
+		if os.path.isfile(file_name) == False:
+			f = open(file_name, 'a')
+			f.write(self.title + "\n")
+			f.close()
+
+		#log
+		bids = self.getBids()
+		log = self.getTimestamp()
+
+		for currency,price in bids.items():
+			log = log + ","+ price
+		
+		log = log + "\n"
+
 		print log
+		f = open(file_name, 'a')
 		f.write(log)
 		f.close()
 
@@ -41,6 +70,10 @@ def thread_write():
 	c.write_log()
 	t = threading.Timer(5,thread_write)
 	t.start()
+	bits = c.getBids()
+
+	print bits
+
 	del c
 
 
